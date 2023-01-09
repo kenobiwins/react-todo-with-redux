c7b720267ebb5629e77e6b20d4a0a8291374162c
Max Sidorenko <fivemoshr@gmail.com>
2023-01-09 19:51:27 +0200

    add files

---
+++ src/redux/store.js
@@ -0,0 +1,6 @@
+import { devToolsEnhancer } from '@redux-devtools/extension';
+import { createStore } from 'redux';
+import { rootReducer } from './reducer';
+
+const enhancer = devToolsEnhancer();
+export const store = createStore(rootReducer, enhancer);
